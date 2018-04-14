31c5ef47c2ab94729e5749d610b4f7935b7bd745
TheSphereWizard <brian.horse@gmail.com>
2018-04-14 11:01:53 -0400

    red

---
+++ src/SendMail.java
@@ -0,0 +1,52 @@
+import java.io.UnsupportedEncodingException;
+import java.util.Properties;
+
+import javax.mail.Message;
+import javax.mail.MessagingException;
+import javax.mail.PasswordAuthentication;
+import javax.mail.Session;
+import javax.mail.Transport;
+import javax.mail.internet.AddressException;
+import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeMessage;
+
+
+public class SendMail {
+
+    public static void main(String[] args) {
+
+        final String username = "your_user_name@gmail.com";
+        final String password = "yourpassword";
+
+        Properties props = new Properties();
+        props.put("mail.smtp.starttls.enable", "true");
+        props.put("mail.smtp.auth", "true");
+        props.put("mail.smtp.host", "smtp.gmail.com");
+        props.put("mail.smtp.port", "587");
+
+        Session session = Session.getInstance(props,
+          new javax.mail.Authenticator() {
+            protected PasswordAuthentication getPasswordAuthentication() {
+                return new PasswordAuthentication(username, password);
+            }
+          });
+
+        try {
+
+            Message message = new MimeMessage(session);
+            message.setFrom(new InternetAddress("your_user_name@gmail.com"));
+            message.setRecipients(Message.RecipientType.TO,
+                InternetAddress.parse("to_email_address@domain.com"));
+            message.setSubject("Testing Subject");
+            message.setText("Dear Mail Crawler,"
+                + "\n\n No spam to my email, please!");
+
+            Transport.send(message);
+
+            System.out.println("Done");
+
+        } catch (MessagingException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
